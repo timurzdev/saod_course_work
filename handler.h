3f99743076a57542c85b585a8b6c58149f17cd24
moskalt <58132831+moskalt@users.noreply.github.com>
2020-09-30 21:02:40 +0700

    added menu method

--- handler.h
+++ handler.h
@@ -7,30 +7,78 @@ private:
 	struct record
 	{
 		char fullName[30]; //30 bytes
-		unsigned short int number; //2 bytes
+		short int number; //2 bytes
 		char position[22]; //22 bytes
 		char birth_date[10]; //10 bytes
 	};
 	int lenght;
 	record* records_array = new record[4000];
+	record** records_index_array = new record * [4000];
 public:
 	Handler() {
 		lenght = 0;
 	}
 	~Handler() {
 		delete[] records_array;
+		delete[] records_index_array;
 	}
-	unsigned short int BytesToInt(unsigned char* buff) {
+	short int BytesToInt(unsigned char* buff) {
 		unsigned char high = buff[0];
 		unsigned char low = buff[1];
 		return high + (low & 0xFF >> (CHAR_BIT));
 	}
+	char* DeleteSpacesInString(char* string) {
+		for (size_t i = 0; i < std::strlen(string); i++) {
+			if (string[i] == ' ') {
+				string[i] = '_';
+			}
+		}
+		return string;
+	}
+	void DeleteSpacesInStruct() {
+		for (size_t i = 0; i < 4000; i++) {
+			strcpy_s(this->records_array[i].fullName, std::strlen(this->records_array[i].fullName) + 1, DeleteSpacesInString(this->records_array[i].fullName));
+			strcpy_s(this->records_array[i].position, std::strlen(this->records_array[i].position) + 1, DeleteSpacesInString(this->records_array[i].position));
+		}
+	}
+	void Menu() {
+		while (true) {
+			std::cout << std::endl;
+			int menu;
+			char filename[] = "./Debug/testBase2.dat";
+			std::cout << "0 - exit menu " << std::endl;
+			std::cout << "1 - load data " << std::endl;
+			std::cout << "2 - print data " << std::endl;
+			std::cout << "Input here: ";
+			std::cin >> menu;
+			std::cout << std::endl;
+			switch (menu)
+			{
+			case 0:
+				return;
+			case 1:
+				GetDataFromFile(filename);
+				DeleteSpacesInStruct();
+				break;
+			case 2:
+				PrintStruct(0);
+				break;
+			default:
+				break;
+			}
+		}
+	}
 	bool GetDataFromFile(char* filename) {
+		if (this->records_array[0].number == 130) {
+			std::cout << "data is already loaded" << std::endl;
+			return false;
+		}
 		char fullName_t[30];
 		unsigned short int number_t;
 		char position_t[22];
 		char birth_date_t[10];
 		char* memblock;
+		char* buff_string = new char[64];
 		int size;
 		unsigned char buff[4];
 		int strlen;
@@ -53,6 +101,7 @@ public:
 			file.read(memblock, size);
 			//filling struct
 			for (size_t i = 0; i < 4000; i++) {
+				this->records_index_array[i] = &this->records_array[i];
 				for (size_t j = memblock_index, temp_index = 0; j < memblock_index + 30; j++, temp_index++ ) {
 					fullName_t[temp_index] = memblock[j];
 				}
@@ -77,27 +126,44 @@ public:
 				strcpy_s(this->records_array[i].birth_date, strlen + 1, birth_date_t);
 				memblock += 10;
 			}
-			//delete memblock
-			//delete[] memblock;
+			//for (size_t i = 0; i < 2; i++) {
+			//	for (size_t j = memblock_index, temp_index = 0; j < memblock_index + 64; j++, temp_index++) {
+			//		buff_string[temp_index] = memblock[memblock_index];
+			//	}
+			//	//fill_struct
+			//	memblock_index += 64;
+			//}
 			file.close();
-
 		}
 		return true;
 	}
 
-	void PrintStruct() {
+	void PrintStruct(size_t start) {
 		std::cout << std::endl;
-		for (size_t i = 0; i < 100; i++) {
-			std::cout.width(30);
-			std::cout << this->records_array[i].fullName << " ";
-			std::cout.width(10);
-			std::cout << this->records_array[i].number << " ";
-			std::cout.width(22);
-			std::cout << this->records_array[i].position << " ";
-			std::cout.width(10);
-			std::cout << this->records_array[i].birth_date << std::endl;
+		system("CLS");
+		size_t end = start + 20;
+		int check;
+		if (this->records_array[0].number != 130) {
+			std::cout << "Array is empty " << std::endl;
+			return;
 		}
-		
+		for (size_t i = start; i < end; i++) {
+			std::cout.width(30);
+			std::cout << this->records_index_array[i]->fullName << " ";
+			std::cout.width(10);
+			std::cout << this->records_index_array[i]->number << " ";
+			std::cout.width(22);
+			std::cout << this->records_index_array[i]->position << " ";
+			std::cout.width(10);
+			std::cout << this->records_index_array[i]->birth_date << std::endl;
+		}
+		std::cout << "Print next 20? (1/0) : ";
+		std::cin >> check;
+		if (check) {
+			PrintStruct(start + 20);
+		}
+		system("CLS");
+		return;
 	}
 	
 };
--- saod_course_work.cpp
+++ saod_course_work.cpp
@@ -6,8 +6,6 @@ int main()
 {
     setlocale(LC_ALL, "ru_RU.CP1251");
     Handler handler;
-    char filename []= "./Debug/testBase2.dat";
-    handler.GetDataFromFile(filename);
-    handler.PrintStruct();
+    handler.Menu();
 }
 
